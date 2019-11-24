80970a85c51e44cd74f6eef08475114569e53676
Adam Klein <adam@500tech.com>
2019-11-24 17:54:40 +0100

    solution

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 
 function App() {
   const [name, setName] = useState("");
   const [results, setResults] = useState("");
 
+  const savedName = useRef();
+  useEffect(function updateSavedName() {
+    savedName.current = name;
+  });
+
   useEffect(function fetchResults() {
     if (name) {
       setTimeout(() => {
         // mock server call
-        setResults(`Results for ${name}`);
+        if (name === savedName.current) {
+          setResults(`Results for ${name}`);
+        }
       }, 2000);
     } else {
       setResults([]);
